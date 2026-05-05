90089402f990bcd2e5997b299473a333b0b0f435
astro <oceanrabbit0101@protonmail.com>
2026-05-06 00:28:01 +0200

    added gen

---
+++ src-tauri/gen/android/app/src/main/java/com/astro/lockin_today/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.astro.lockin_today
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
